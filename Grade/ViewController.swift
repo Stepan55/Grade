8a56da47562ebf5d5fff2bc2ddd4dd371fdfb40d
Stepan55 <stepan.volk.91@mail.ru>
2023-01-03 02:14:54 +0300

    done Compact/Regular SCREENS

--- Grade/ViewController.swift
+++ Grade/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var farenheitLabel: UILabel!
+    @IBOutlet weak var kelvinLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        slider.value = 0.5
+        slider.minimumValue = 0
+        slider.maximumValue = 1
     }
     
-    /// ds 
+    @IBAction func slideAction(_ sender: UISlider) {
+        
+    }
+    
 
 }
 
